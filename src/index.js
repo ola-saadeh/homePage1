36ea82cc5074a962183b9465be2afecbf6e412e4
ola saadeh <71474457+ola-saadeh@users.noreply.github.com>
2023-12-12 20:50:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import { BrowserRouter } from 'react-router-dom';
+import "../node_modules/animate.css/animate.min.css";
+import "../node_modules/animate.css/animate.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
